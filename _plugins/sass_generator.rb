888787c2bc1ecc48d71c924b86d1c9e5f0947ad6
Steven Alexander <steven.william.alexander@googlemail.com>
2013-05-20 12:47:56 +0100

    added site stylesheet

--- _plugins/sass_generator.rb
+++ _plugins/sass_generator.rb
@@ -1,11 +1,22 @@
 module Jekyll
-
-  class SassGenerator < Generator
-    safe true
-
-    def generate(site)
-      puts "generating sass"
-    end
-  end
-
+	require 'sass'
+	class SassGenerator < Generator
+		safe true
+		priority :low
+		
+		def generate(site)
+			begin
+        t = Time.now.strftime("%Y-%m-%d %H:%M:%S");
+        puts "[#{t}] Performing Sass Conversion."
+        sass_filename = "./_assets/stylesheets/main.scss"
+        css = Sass::Engine.for_file(sass_filename, 
+        	:syntax => :scss, 
+        	:load_paths => ["./_assets/stylesheets", "./_submodules/toolkit/stylesheets"], 
+        	:line_numbers => true).render
+        File.open("./assets/stylesheets/main.css", "wb") {|f| f.write(css) }
+      rescue StandardError => e
+        puts "[#{t}] !!! SASS Error - [Line #{e.sass_line}] -- #{e.to_s}"
+      end
+		end
+	end
 end
