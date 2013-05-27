707d138224447b00aee3cf33b96f36d75c120cc4
Steven Alexander <steven.william.alexander@googlemail.com>
2013-05-27 17:49:49 +0100

    added regen check to stop repeating auto-regens

--- _plugins/sass_generator.rb
+++ _plugins/sass_generator.rb
@@ -3,17 +3,23 @@ module Jekyll
 	class SassGenerator < Generator
 		safe true
 		priority :low
-		
+
 		def generate(site)
 			begin
+        main_file_path = "./assets/stylesheets/main.css"
+
+        # check that the file hasn't been generated recently to prevent chained auto regens
+        puts "check!:", (Time.now.to_i - File.mtime(main_file_path).to_i) < 3
+        return nil if (Time.now.to_i - File.mtime(main_file_path).to_i) < 3
+
         t = Time.now.strftime("%Y-%m-%d %H:%M:%S");
         puts "[#{t}] Performing Sass Conversion."
         sass_filename = "./_assets/stylesheets/main.scss"
-        css = Sass::Engine.for_file(sass_filename, 
-        	:syntax => :scss, 
-        	:load_paths => ["./_assets/stylesheets", "./_submodules/toolkit/stylesheets"], 
+        css = Sass::Engine.for_file(sass_filename,
+        	:syntax => :scss,
+        	:load_paths => ["./_assets/stylesheets", "./_submodules/toolkit/stylesheets"],
         	:line_numbers => true).render
-        File.open("./assets/stylesheets/main.css", "wb") {|f| f.write(css) }
+        File.open(main_file_path, "wb") {|f| f.write(css) }
       rescue StandardError => e
         puts "[#{t}] !!! SASS Error - [Line #{e.sass_line}] -- #{e.to_s}"
       end
