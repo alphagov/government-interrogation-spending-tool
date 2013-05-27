fc8ea7ec38356e462c040607abc9649b553e3c9f
Steven Alexander <steven.william.alexander@googlemail.com>
2013-05-27 15:14:15 +0100

    updated path to test data

--- _process_test_data.sh
+++ _process_test_data.sh
@@ -1,5 +1,5 @@
 echo "Processing test data to create sample pages with data"
 rm -rf qds/*
 rm -rf oscar/*
-ruby -r "./_processors/qds_processor.rb" -e "QdsProcessor.new.process '_processors/spec/test_data/test_qds.csv'"
-ruby -r "./_processors/oscar_processor.rb" -e "OscarProcessor.new.process '_processors/spec/test_data/test_oscar.csv'"
+ruby -r "./_processors/qds_processor.rb" -e "QdsProcessor.new.process '_spec/test_data/test_qds.csv'"
+ruby -r "./_processors/oscar_processor.rb" -e "OscarProcessor.new.process '_spec/test_data/test_oscar.csv'"
