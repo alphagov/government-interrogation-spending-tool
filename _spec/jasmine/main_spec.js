6204c1e86ffd0d0e06c9f7e908f31e97c29d5230
Steven Alexander <steven.william.alexander@googlemail.com>
2013-05-27 18:26:38 +0100

    added tests for loading visualisations

--- _spec/jasmine/main_spec.js
+++ _spec/jasmine/main_spec.js
@@ -1,7 +1,36 @@
-describe("Main", function() {
-  var main;
+describe("Util", function() {
 
-  it("should be able to play a Song", function() {
-    expect("1").toEqual("1");
+  var util = new gist.utils.Util();
+
+  describe("load_visualisations", function () {
+
+    describe("when there is no chart div on page", function () {
+      it("should not call load_from_table", function () {
+        spyOn(util,'load_data_from_table').andReturn([]);
+
+        util.load_visualisations();
+
+        expect(util.load_data_from_table).not.toHaveBeenCalled();
+      });
+    });
+
+    describe("when there is a chart div on page", function () {
+      beforeEach(function () {
+        loadFixtures('visualisation-divs.html');
+        spyOn(util,'load_data_from_table').andReturn([]);
+        spyOn(util,'draw_tree_map');
+
+        util.load_visualisations();
+      });
+
+      it("should call load_from_table", function () {
+        expect(util.load_data_from_table).toHaveBeenCalled();
+      });
+
+      it("should call draw against the treemap", function () {
+        expect(util.draw_tree_map).toHaveBeenCalled();
+      });
+    });
   });
+
 });
--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
-  console.log("ready");
-  $(".visualisation").each(function(index, node) {
-    if ($(node).hasClass('treemap')) {
+  var util = new gist.utils.Util();
+  util.load_visualisations();
+});
+
+var gist = gist || {};
+gist.utils = gist.utils || (function() {
+  var util_obj = function util() {};
+
+  $.extend(util_obj.prototype, {
+
+    load_visualisations : function() {
+
+      if ($("#chart").length != 0) {
+        var that = this,
+            table_data = this.load_data_from_table('data-table');
+        $(".visualisation").each(function(index, node) {
+          if ($(node).hasClass('treemap')) { that.draw_tree_map(node, table_data); }
+        });
+      }
+    },
+
+    load_data_from_table : function(table_id) {
+      return [];
+    },
+
+    draw_tree_map : function(node, chart_data) {
       new gist.charts.treemap.Widget(node, { chart_data: null }).draw(940, 400);
     }
+
   });
-});
+
+  return {
+    Util : util_obj
+  };
+})();
