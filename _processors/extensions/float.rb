c3a7f60d09af75df5189b6093b26b4af9ce65201
Steven Alexander <steven.william.alexander@googlemail.com>
2013-05-28 13:26:32 +0100

    added magnitude helper

---
+++ _processors/extensions/float.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+class Float < Numeric
+  def to_magnitude_string
+    val = self.to_f
+    abs = val.abs
+    result = sprintf('%.1f', val)
+
+    if    abs >= 1000000000000
+      result = sprintf('%.1f', val/1000000000000) + "tn"
+    elsif abs >= 1000000000
+      result = sprintf('%.1f', val/1000000000) + "bn"
+    elsif abs >= 1000000
+      result = sprintf('%.1f', val/1000000) + "m"
+    elsif abs >= 1000
+      result = sprintf('%.1f', val/1000) + "k"
+    end
+
+    result.gsub(/([0-9]{3})(\.[0-9]+)([a-z]*)/, '\1\3').sub(".0","")
+  end
+
+  def to_sterling_magnitude_string
+    result = "£" + self.to_magnitude_string
+    result.sub("£-", "-£")
+  end
+end
---
+++ _spec/extensions/float_spec.rb
@@ -0,0 +1,55 @@
+# encoding: utf-8
+require_relative "../../_processors/extensions/float"
+
+describe "Float" do
+  describe "to_magnitude_string" do
+    it "should return 0 for 0.0" do
+      0.0.to_magnitude_string.should eq "0"
+    end
+    it "should return 0.1 for 0.099999" do
+      0.099999.to_magnitude_string.should eq "0.1"
+    end
+    it "should return 0.1 for 0.101" do
+      0.101.to_magnitude_string.should eq "0.1"
+    end
+    it "should return 1 for 1.01" do
+      1.01.to_magnitude_string.should eq "1"
+    end
+    it "should return 999 for 999.1" do
+      999.1.to_magnitude_string.should eq "999"
+    end
+    it "should return 9k for 9049.0" do
+      9049.0.to_magnitude_string.should eq "9k"
+    end
+    it "should return 9.9k for 9899.0" do
+      9899.0.to_magnitude_string.should eq "9.9k"
+    end
+    it "should return 1.1m for 1100000.0" do
+      1100000.0.to_magnitude_string.should eq "1.1m"
+    end
+    it "should return 99.9m for 99900000.0" do
+      99900000.0.to_magnitude_string.should eq "99.9m"
+    end
+    it "should return 9bn for 9000000000.0" do
+      9000000000.0.to_magnitude_string.should eq "9bn"
+    end
+    it "should return 9tn for 9000000000000.0" do
+      9000000000000.0.to_magnitude_string.should eq "9tn"
+    end
+    it "should return -9k for -9049.0" do
+      -9049.0.to_magnitude_string.should eq "-9k"
+    end
+  end
+
+  describe "to_sterling_magnitude_string" do
+    it "should return £0 for 0.0" do
+      0.0.to_sterling_magnitude_string.should eq "£0"
+    end
+    it "should return £1.1m for 1100000.0" do
+      1100000.0.to_sterling_magnitude_string.should eq "£1.1m"
+    end
+    it "should return -£9k for -9049.0" do
+      -9049.0.to_sterling_magnitude_string.should eq "-£9k"
+    end
+  end
+end
