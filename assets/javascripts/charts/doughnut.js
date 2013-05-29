9d0fb1998849c4ff886ba84007cda986ad0f70cb
Steven Alexander <steven.william.alexander@googlemail.com>
2013-05-29 15:39:00 +0100

    basic doughnut chart

--- assets/javascripts/charts/doughnut.js
+++ assets/javascripts/charts/doughnut.js
@@ -55,7 +55,42 @@ gist.charts.doughnut = gist.charts.doughnut || (function() {
       this.height = h;
 
       if (this.opts.chart_data) {
-        console.log("doughnut!")
+        var data = this.opts.chart_data.filter(function(d) { return d.total > 0; })
+          .sort(function(a,b) {return (a.total < b.total)? 1 : (a.total == b.total)? 0 : -1; });
+
+        var radius = Math.min(width, height) / 2;
+        var svg = d3.select("#" + node_id).append("svg:svg")
+          .attr("width", w)
+          .attr("height", h);
+
+        var chart_g = svg.append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+        var vis = chart_g.append("g")
+          .attr("transform", "translate(" + radius + "," + radius + ")");
+
+        var partition = d3.layout.partition()
+          .sort(null)
+          .size([2 * Math.PI, radius * radius])
+          .value(function(d) { return d.total; })
+          .children(function(d) { return d.values; });
+
+        var arc = d3.svg.arc()
+          .startAngle(function(d) { return d.x; })
+          .endAngle(function(d) { return d.x + d.dx; })
+          .innerRadius(function(d) { return Math.sqrt(d.y); })
+          .outerRadius(function(d,i) { return Math.sqrt(d.y + d.dy); });
+
+        var root = { "name": "root", "values": data };
+
+        var path = vis.data([root]).selectAll("path")
+          .data(partition.nodes)
+          .enter().append("path")
+            .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
+            .attr("d", arc)
+            .attr("fill-rule", "evenodd")
+            .style("stroke", "#fff")
+            .style("fill", function(d) { return "blue"; });
       }
     }
   });
