992e02c52af4aa193a027bfd850ab4cfcc396360
Steven Alexander <steven.william.alexander@googlemail.com>
2013-06-04 13:54:53 +0100

    added colours to department map

--- _spec/department_mapper_spec.rb
+++ _spec/department_mapper_spec.rb
@@ -3,12 +3,12 @@ require_relative "../_processors/department_mapper.rb"
 
 describe "DepartmentMapper" do
   describe "map_raw_to_css_hash" do
-    it "takes a raw department reference and returns a hash with department abbr, name, css class, css logo suffix" do
-      co_hash = { :abbr => "CO", :name => "Cabinet Office", :css_class => "cabinet-office", :css_logo_suffix => "single-identity"}
+    it "takes a raw department reference and returns a hash with department abbr, name, css class, css logo suffix, colour, font_colour" do
+      co_hash = { :abbr => "CO", :name => "Cabinet Office", :css_class => "cabinet-office", :css_logo_suffix => "single-identity", :colour=>"#0078ba", :font_colour=>"#fff"}
       DepartmentMapper.map_raw_to_css_hash("Cabinet Office").should eq(co_hash)
       DepartmentMapper.map_raw_to_css_hash("CO").should eq(co_hash)
 
-      dfid_hash = { :abbr => "DFID", :name => "Department for International Development", :css_class => "department-for-international-development", :css_logo_suffix => "single-identity" }
+      dfid_hash = { :abbr => "DFID", :name => "Department for International Development", :css_class => "department-for-international-development", :css_logo_suffix => "single-identity", :colour=>"#729ecd", :font_colour=>"#0A0C0C" }
       DepartmentMapper.map_raw_to_css_hash("Department for International Development").should eq(dfid_hash)
       DepartmentMapper.map_raw_to_css_hash("DfID").should eq(dfid_hash)
       DepartmentMapper.map_raw_to_css_hash("DfID").should eq(dfid_hash)
