8f2dcc1f33f7ef99a4419fe535a7f0c71d77678d
Steven Alexander <steven.william.alexander@googlemail.com>
2013-06-07 15:01:29 +0100

    removed line causing error if main.css doesnt exist

--- _plugins/sass_generator.rb
+++ _plugins/sass_generator.rb
@@ -8,9 +8,6 @@ module Jekyll
 			begin
         main_file_path = "./assets/stylesheets/main.css"
 
-        # check that the file hasn't been generated recently to prevent chained auto regens
-        return nil if (Time.now.to_i - File.mtime(main_file_path).to_i) < 3
-
         t = Time.now.strftime("%Y-%m-%d %H:%M:%S");
         puts "[#{t}] Performing Sass Conversion."
         sass_filename = "./_assets/stylesheets/main.scss"
@@ -18,7 +15,8 @@ module Jekyll
         	:syntax => :scss,
         	:load_paths => ["./_assets/stylesheets", "./_submodules/toolkit/stylesheets"],
         	:line_numbers => true).render
-        File.open(main_file_path, "wb") {|f| f.write(css) }
+
+        File.open(main_file_path, "w+") {|f| f.write(css) }
       rescue StandardError => e
         if e.class.method_defined? :sass_line
           puts "[#{t}] !!! SASS Error - [Line #{e.sass_line}] -- #{e.to_s}"
